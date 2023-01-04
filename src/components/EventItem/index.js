26ae4ee83a42cc7a12757e834093e374acc9e9fc
bhasa <bhaskar19191@gmail.com>
2023-01-04 06:24:49 +0000

    events

---
+++ src/components/EventItem/index.js
@@ -0,0 +1,29 @@
+// Write your code here
+import './index.css'
+
+const EventItem = props => {
+  const {eventDetails, setActiveEventId, isActive} = props
+  const {imageUrl, name, location, id} = eventDetails
+  const eventImgClassName = isActive ? 'active event-image' : 'event-image'
+
+  const onClickEvent = () => {
+    setActiveEventId(id)
+  }
+
+  return (
+    <li className="event-item">
+      <button type="button" className="event-button">
+        <img
+          src={imageUrl}
+          alt="event"
+          className={eventImgClassName}
+          onClick={onClickEvent}
+        />
+      </button>
+      <p className="name">{name}</p>
+      <p className="location">{location}</p>
+    </li>
+  )
+}
+
+export default EventItem
